800a533142f4816a9c8204e89aaf7f9eced88afb
Lab Technologies <labtechnologies@Labs-MacBook-Pro.local>
2025-10-19 17:36:16 -0600

    adding semana 15

--- semana_15/bubble_sort.py
+++ semana_15/bubble_sort.py
@@ -1,19 +1,19 @@
 def reverse_bubble_sort(list_to_sort):
-	for repetition in range(len(list_to_sort)):  ## O(n)
-		flag = False ## O(1)
-		for index in range(len(list_to_sort) - 1 , repetition , -1):  ## O(n^2)
-			current_number = list_to_sort[index] ## O(1)
-			next_number = list_to_sort[index - 1] ## O(1)
-			print(f"Repetition: {repetition} -- Index:{index} -- Current number: {current_number} -- Next number : {next_number} ") ## O(1)
-			if current_number > next_number: ## O(1)
-				print('El elemento actual es mayor al siguiente. Intercambiandolos...') ## O(1)
-				list_to_sort[index] = next_number ## O(1)
-				list_to_sort[index - 1] = current_number ## O(1)
-				flag = True ## O(1)
+	for repetition in range(len(list_to_sort)):
+		flag = False 
+		for index in range(len(list_to_sort) - 1 , repetition , -1):
+			current_number = list_to_sort[index]
+			next_number = list_to_sort[index - 1]
+			print(f"Repetition: {repetition} -- Index:{index} -- Current number: {current_number} -- Next number : {next_number} ")
+			if current_number > next_number:
+				print('El elemento actual es mayor al siguiente. Intercambiandolos...')
+				list_to_sort[index] = next_number
+				list_to_sort[index - 1] = current_number
+				flag = True 
 			
-		if not flag: ## O(1)
-			return ## O(1)
+		if not flag:
+			return
 
-list_to_sort = [1, 8 , 10, 2, 7, 4, 9 ] ## O(1)
-reverse_bubble_sort(list_to_sort) ## O(1)
-print(list_to_sort) ## O(1)
+list_to_sort = [1, 8 , 10, 2, 7, 4, 9 ]
+reverse_bubble_sort(list_to_sort)
+print(list_to_sort)
